bec23928806daf8f76b2c472e030e15c799865a7
vicky <vicky@VIVEK>
2020-05-03 23:34:35 +0530

    new added

---
+++ DataBaseFirstDemo/DataBaseFirstDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY_FRAMEWORK2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new ENTITY_FRAMEWORK2020Entities2();
+            var post = new Post()
+            {
+                Body = "Body",
+                DatePublished = DateTime.Now,
+                Title = "Neo",
+                PostID = 1
+            };
+            context.Posts.Add(post);
+            context.SaveChanges();
+
+        }
+    }
+}
