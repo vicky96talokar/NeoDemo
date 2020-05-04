73b57d4078a377da322fe41079692055d50a00be
vicky <vicky@VIVEK>
2020-05-04 12:27:02 +0530

    Added

---
+++ DBFirst/DBFirst/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBFirst
+{
+    public enum Level : byte
+    {
+        Beginner = 1,
+        Intermediate = 2,
+        Advanced = 3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var course = new Course();
+            course.Level = CourseLevel.Beginner;//1
+
+        }
+    }
+}
